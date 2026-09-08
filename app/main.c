66656c55e082809d38f1c15f01dd258b9dbfdc59
Adrian <gromkov2002@gmail.com>
2026-09-08 11:51:12 +0300

    Цифры на матрице 1-6

--- app/main.c
+++ app/main.c
@@ -30,9 +30,22 @@
 // Частота мигания светодиода, Гц (полных циклов вкл/выкл в секунду)
 #define BLINK_FREQ_HZ 10
 
+// Шрифт цифр 1-5 для матрицы 8x8 (по строкам, старший бит - левый столбец)
+static const uint8_t digit_font[5][8] = {
+	{0x18, 0x38, 0x18, 0x18, 0x18, 0x18, 0x18, 0x3C}, // 1
+	{0x78, 0x84, 0x04, 0x08, 0x10, 0x20, 0x40, 0xFC}, // 2
+	{0x78, 0x84, 0x04, 0x38, 0x04, 0x84, 0x84, 0x78}, // 3
+	{0x08, 0x18, 0x28, 0x48, 0x88, 0xFC, 0x08, 0x08}, // 4
+	{0xFC, 0x80, 0x80, 0xF8, 0x04, 0x84, 0x84, 0x78}, // 5
+};
 
+// Картинка на 6-е нажатие (заказанный пользователем узор)
+static const uint8_t picture6[8] = {
+	0x18, 0x18, 0x18, 0x18, 0x18, 0x7E, 0x7E, 0x7E,
+};
 
-
+// 0 - бегущий огонёк, 1..5 - цифра, 6 - картинка picture6
+static volatile uint8_t digit_mode = 0;
 void TMR0_IRQHandler();
 
 void TMR0_init(uint32_t period)
@@ -83,9 +96,31 @@ int main(void)
   // нужно 2 срабатывания -> частота таймера в 2 раза выше BLINK_FREQ_HZ
   TMR0_init(SystemCoreClock/(BLINK_FREQ_HZ*2));
   InterruptEnable();
+
+  // Уровень кнопки USER BTN (PA13) в состоянии покоя - определяем при
+  // старте, чтобы не зависеть от того, притянута она к 0 или к 1
+  uint8_t btn_idle_level = (GPIOA->DATA & BTN_PIN_MSK) ? 1 : 0;
+  uint8_t btn_pressed = 0;
+
   while(1)
   {
+    uint8_t btn_level = (GPIOA->DATA & BTN_PIN_MSK) ? 1 : 0;
 
+    if (btn_level != btn_idle_level && !btn_pressed)
+    {
+      delay(50000); // антидребезг
+      if (((GPIOA->DATA & BTN_PIN_MSK) ? 1 : 0) != btn_idle_level)
+      {
+        btn_pressed = 1;
+        digit_mode = (digit_mode % 6) + 1;
+        MAX7219_SetBuffer(digit_mode == 6 ? picture6 : digit_font[digit_mode - 1]);
+        printf("Button pressed, digit=%d\r\n", digit_mode);
+      }
+    }
+    else if (btn_level == btn_idle_level)
+    {
+      btn_pressed = 0;
+    }
   }
 
   return 0;
@@ -97,10 +132,13 @@ void TMR0_IRQHandler()
 {
 	BSP_LED_Toggle();
 
-	// "Бегущий огонёк" по одной строке матрицы MAX7219
-	static uint8_t col = 0;
-	MAX7219_SetRow(1, 1 << col);
-	col = (col + 1) % 8;
+	// "Бегущий огонёк" по одной строке матрицы MAX7219, пока не показана цифра
+	if (!digit_mode)
+	{
+		static uint8_t col = 0;
+		MAX7219_SetRow(1, 1 << col);
+		col = (col + 1) % 8;
+	}
 
     //Сбрасываем флаг прерывания таймера
     TMR0->IC = 1;
